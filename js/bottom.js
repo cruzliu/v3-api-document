8bd4c6569e2fe2e50b4bc361686c8d2eaf1434ea
Cruz <cruz@Cruzde-MacBook-Pro.local>
2018-07-17 15:01:03 +0800

    f3

--- js/bottom.js
+++ js/bottom.js
@@ -1,7 +1,7 @@
 /**
  * @api {get} /api/bottomBar_api 取得bottom bar當下檔期資料
  * @apiName GetBottomBar
- * @apiGroup Bottom
+ * @apiGroup BottomBar
  *
  * @apiParam {Integer} device App:1, Mobile Web:2
  *
@@ -13,17 +13,27 @@
  * @apiSuccess {String} end_ts 結束日期
  *
  * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "firstname": "John",
- *       "lastname": "Doe"
- *     }
+*    {
+*     "data": [
+*        {
+*          "name": "所有好康",
+*          "link": "https://www.buy123.com.tw",
+*          "sorting": "1",
+*          "start_ts": "2018-07-01 00:00:00",
+*          "end_ts": "2018-07-31 23:59:59",
+*          "default_image": "https://s3-us-west-1.amazonaws.com/s3.buy123.com.tw/bottom_bar/default_image/cc083e35fb0f36ca4384e2d9dbbdae99.png",
+*          "pressed_image": "https://s3-us-west-1.amazonaws.com/s3.buy123.com.tw/bottom_bar/pressed_image/cd1c054925fb4d86d7ffe5dd484f2846.png"
+*        },
+*        {
+*          "name": "我的最愛12",
+*          "link": "https://m.buy123.com.tw/site/favorite12",
+*          "sorting": "2",
+*          "start_ts": "2018-07-03 00:00:00",
+*          "end_ts": "2018-07-29 23:59:59",
+*          "default_image": "https://s3-us-west-1.amazonaws.com/s3.buy123.com.tw/bottom_bar/default_image/c0e114ab315763726c9e98cfa89a2013.png",
+*          "pressed_image": "https://s3-us-west-1.amazonaws.com/s3.buy123.com.tw/bottom_bar/pressed_image/9126c56e20a0e969bb1341a335bf39f8.png"
+*        }
+*      ]
+*    }
  *
- * @apiError UserNotFound The id of the User was not found.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "UserNotFound"
- *     }
  */
